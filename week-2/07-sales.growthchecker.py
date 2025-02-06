74f86843059564ee4b1dd02cafcd1e70f40afc4a
laolu2 <olaadekso@gmail.com>
2025-02-06 13:36:16 +0100

    modified excercise 7

--- week-2/07-sales.growthchecker.py
+++ week-2/07-sales.growthchecker.py
@@ -17,8 +17,7 @@ year_1_sales = [4000, 4500, 4700, 5000, 5200, 4800]
 year_2_sales = [4200, 4600, 4800, 5100, 5300, 4900]
 
 for month, (year_1_sales, year_2_sales) in enumerate(zip(year_1_sales, year_2_sales), start=1):
-    growth_percentage = ((year_2_sales - year_1_sales) / year_1_sales) * 100
-    if growth_percentage > 5:
-        print(f"Month {month}: Growth = {growth_percentage:.2f}%")
+         growth_percentage = ((year_2_sales - year_1_sales) / year_1_sales) * 100
+print(f"Month {month}: Growth = {growth_percentage:.2f}%")
 
 
